b3d55d71933c1d92250b30a2dfd8c455b6032837
Tony <anthonymostrowski@gmail.com>
2024-07-09 14:40:32 -0400

    your

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,19 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mailgun.org',
+    port:                 587,
+    domain:               'sandboxc8f5668c76f1437ebf6571563ad5c674.mailgun.org',
+    user_name:            'postmaster@sandboxc8f5668c76f1437ebf6571563ad5c674.mailgun.org',
+    password:             'af99342015d4bc900e375868d14a876c-8a084751-9bcecc48',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -44,19 +55,19 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # Raises error for missing translations
+  # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  # Don't overwrite Rspec files in generators
+  # Don't overwrite Rspec files in generators.
   config.generators do |g|
     g.test_framework nil
   end
 
-  # Using the Amazon S3 settings for Active Storage
+  # Using the Amazon S3 settings for Active Storage.
   config.active_storage.service = :amazon
 end
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,10 +37,10 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
-  # Raises error for missing translations
+  # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
-  # ActiveStorage test service configuration
+  # ActiveStorage test service configuration.
   config.active_storage.service = :test
 end
 
